558dde71739ad811cf20f87e3b219a38c5d4c2c5
Braveheart570 <42154318+Braveheart570@users.noreply.github.com>
2025-01-23 19:06:00 -0500

    All Start Screen animations working

--- SDL_Template/StartScreen.cpp
+++ SDL_Template/StartScreen.cpp
@@ -12,7 +12,7 @@ StartScreen::StartScreen() {
 	}
 
 	for (int c = 0; c < sMountainPannels; c++) {
-		if (c == 1) {
+		if (c == 0) {
 			mMountains[c] = new GLTexture("MountainsCastle.png");
 		}
 		else {
@@ -20,19 +20,16 @@ StartScreen::StartScreen() {
 		}
 		
 		mMountains[c]->Scale(Vec2_One * 5.0f);
-		mMountains[c]->Position({ (c * 220.0f)-330.0f,300.0f });
 		mMountains[c]->Parent(this);
 	}
 
 
 	mMoon = new GLTexture("Moon.png");
 	mMoon->Scale(Vec2_One * 5);
-	mMoon->Position({Graphics::SCREEN_WIDTH/2,-100.0f});
 	mMoon->Parent(this);
 
 	mTitle = new GLTexture("Title.png");
 	mTitle->Scale(Vec2_One * 5);
-	mTitle->Position({ Graphics::SCREEN_WIDTH / 2,490.0f });
 	mTitle->Parent(this);
 	
 
@@ -56,33 +53,45 @@ StartScreen::~StartScreen() {
 
 void StartScreen::ResetAnimation() {
 
+	for (int c = 0; c < sMountainPannels; c++) {
+		mMountains[c]->Position({ (c * 220.0f) - 330.0f,300.0f });
+	}
 
+	mMoon->Position({ Graphics::SCREEN_WIDTH / 2,-110.0f });
+	mTitle->Position({ Graphics::SCREEN_WIDTH / 2,490.0f });
 
 }
 
 void StartScreen::Update() {
 
+	mMoon->Translate(Vec2_Up * 30.0f * mTimer->DeltaTime());
+	
 	float triggerDistance = 110.0f;
 
-	for (auto tex : mMountains) {
-		tex->Translate(Vec2_Right*mTimer->DeltaTime()*50.0f);
+	for (int c = 0; c < sMountainPannels; c++) {
+		mMountains[c]->Translate(Vec2_Right * mTimer->DeltaTime() * 50.0f);
 
-		if (tex->Position().x >= Graphics::SCREEN_WIDTH + triggerDistance) {
-			tex->Translate(-(Vec2_Right*(220.0f*sMountainPannels)));
+		if (mMountains[c]->Position().x >= Graphics::SCREEN_WIDTH + triggerDistance) {
+			mMountains[c]->Translate(-(Vec2_Right*(220.0f*sMountainPannels)));
+			if (c==0) {
+				mMoon->Position({Graphics::SCREEN_WIDTH/2,-110.0f});
+			}
 		}
 
 	}
+
+	
 
 }
 
 void StartScreen::Render() {
 	for (int c = 0; c <= 2; c++) mSky[c]->Render();
 
-	//mMoon->Render();
+	if (mMoon->Position().y <= Graphics::SCREEN_HEIGHT-180) { mMoon->Render(); }
 
 	for (int c = 0; c < sMountainPannels; c++) mMountains[c]->Render();
 
 	
-	//mTitle->Render();
+	mTitle->Render();
 
 }
